a602be512cd3b75cd0f25b37d74cc27de17432dc
An Nguyen <anngntien@gmail.com>
2025-10-06 22:47:24 +0700

    Create generate problem: Done

--- gen_test.py
+++ gen_test.py
@@ -1,32 +1,67 @@
 import random
+import json
+import os
+import numpy as np
+from typing import List
 
 def generate_matrix(N):
     if N < 2 or N > 24:
-        raise ValueError("N phải nằm trong khoảng từ 2 đến 24")
+        raise ValueError("N in range 4 to 24")
     if (N * N) % 2 != 0:
-        raise ValueError("Số phần tử trong ma trận phải là số chẵn để chia thành cặp")
+        raise ValueError("Number must even!")
 
     num_pairs = (N * N) // 2
     
-    if num_pairs > 23:  
-        raise ValueError("Không thể tạo ma trận vì số cặp vượt quá 23 (số giá trị khả dụng)")
+    entities = []
+    for v in range(num_pairs):
+        entities.append(v)
+        entities.append(v)  
 
-    values = random.sample(range(2, 25), num_pairs)  
-    paired_values = []
-    for v in values:
-        paired_values.extend([v, v])  
+    random.shuffle(entities)
+    field_matrix: List[List[int]] = []
+    for r in range(N):
+        start_index = r * N
+        end_index = (r + 1) * N
+        row = entities[start_index:end_index]
+        field_matrix.append(row)
 
-    random.shuffle(paired_values)
+    return field_matrix
 
-    matrix = [paired_values[i * N:(i + 1) * N] for i in range(N)]
-    return matrix
+def generate_problem(N):
+    template_json = {
+        "startsAt": 0,
+        "problem": {
+            "field": {
+                "size": N,
+                "entities": generate_matrix(N)
+            }
+        }
+    }
+    save_dir = "./Problem"
+    output_json = "problem.json"
+    output_path = os.path.join(save_dir, output_json)
+    if not os.path.exists(save_dir):
+        os.makedirs(save_dir)
+        print(f"Create directory: {save_dir}")
+    try:
+        with open(output_path, "w", encoding='utf-8') as f:
+            json.dump(template_json, f, ensure_ascii=False, indent=4)
+        print(f"Create json sucessful! at {output_path}")
+    except Exception as e:
+        print(f"Error: {e}")
 
 if __name__ == "__main__":
-    N = random.randint(2, 24)
-    while (N * N) % 2 != 0 or (N * N) // 2 > 23:  
-        N = random.randint(2, 24)
+    # N = random.randrange(4, 25, 1)
+    # while (N * N) % 2 != 0 or (N * N) // 2 > 23:  
+    #     N = random.randrange(4, 24, 1)
+    
+    N = 12
+    generate_problem(N)
 
-    print(f"Ma trận {N}x{N}:")
-    mat = generate_matrix(N)
-    for row in mat:
-        print(row)
+    #Just check my code :)
+    # mat = generate_matrix(N)
+    # print(np.array(mat))
+    # print(np.max(mat))
+    # for i in range(np.max(mat)+1):
+    #     count = np.sum(np.array(mat) == i)
+    #     print(f"Element {i} has: {count} number")
