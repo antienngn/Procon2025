59ce85002fae4a94b20964b4e0f797a39cab5e12
An Nguyen <anngntien@gmail.com>
2025-10-07 23:00:05 +0700

    Rotate 90 degree clockwise: In progress

--- solver.py
+++ solver.py
@@ -1,18 +1,20 @@
 import numpy as np
 import json
 import os
-from utils import extract_entities
+from utils import extract_entities, rotate90
 
 file_path = "./Problem/problem.json"
 key_feature = ["problem", "field", "entities"]
 
 def solver():
     garden = extract_entities(file_path=file_path, key_feature=key_feature)
-    return garden
+    temp = rotate90(x_cord=0, y_cord=0, size=2, garden=garden)
+    return temp
 
         
 
 if __name__ == "__main__":
     # print(np.array(extract_entities(file_path=file_path,key_feature=key_feature)))
     # print(solver())
-    solver()
+    test_rotate = solver()
+    print(test_rotate)
--- utils.py
+++ utils.py
@@ -14,3 +14,14 @@ def extract_entities(file_path, key_feature):
         print(f"File not found '{file_path}'.")
 
     return np.array(problem)
+
+def rotate90(x_cord, y_cord, size, garden):
+    for x_cord in range(size):
+        for y_cord in range(x_cord+1, size):
+            garden[x_cord][y_cord], garden[y_cord][x_cord] = garden[y_cord][x_cord], garden[x_cord][y_cord]
+
+    block = garden[x_cord:x_cord+size, y_cord:y_cord+size]
+    for row in block:
+        np.flip(row)
+    
+    return garden
