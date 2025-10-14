f65f76c6761d84f09ee9c8f9ad38b2b5fc157954
An Nguyen <anngntien@gmail.com>
2025-10-14 23:49:09 +0700

    Brute force: In progress 2

--- solver.py
+++ solver.py
@@ -2,9 +2,26 @@ import numpy as np
 import json
 import os
 from utils import extract_entities, rotate90
+from collections import defaultdict
 
 file_path = "./Problem/problem.json"
 key_feature = ["problem", "field", "entities"]
+
+def sum_pair_manhattan(board, n):
+    pos = pair_positions(board)
+    s = 0
+    for v, lst in pos.items():
+        if len(lst) != 2:
+            continue
+        (r1,c1),(r2,c2) = rc(n,lst[0]), rc(n,lst[1])
+        s += abs(r1 - r2) + abs(c1 - c2)
+    return s
+
+def pair_positions(board):
+    pos = defaultdict(list)
+    for i,v in enumerate(board):
+        pos[v].append(i)
+    return pos
 
 def solver():
     garden = extract_entities(file_path=file_path, key_feature=key_feature)
