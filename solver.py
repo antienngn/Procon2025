8d54d81865d844863dce2f68cf5858d3998fa5dd
An Nguyen <anngntien@gmail.com>
2025-10-16 17:37:12 +0700

    Brute force: Hanging yet

--- solver.py
+++ solver.py
@@ -1,43 +1,72 @@
 import numpy as np
 import json
 import os
-from utils import extract_entities, rotate90
+from utils import rotate90, find_partner, select_block 
 from collections import defaultdict
 
 file_path = "./Problem/problem.json"
 key_feature = ["problem", "field", "entities"]
 
-def sum_pair_manhattan(board, n):
-    pos = pair_positions(board)
-    s = 0
-    for v, lst in pos.items():
-        if len(lst) != 2:
-            continue
-        (r1,c1),(r2,c2) = rc(n,lst[0]), rc(n,lst[1])
-        s += abs(r1 - r2) + abs(c1 - c2)
-    return s
+# board= np.array([[47,16,39,48,51,69,8,59,2,70,30,52],
+#          [17,65,46,45,12,46,29,43,36,40,65,47],
+#          [54,1,4,26,54,27,32,57,41,23,29,13],
+#          [52,14,31,9,16,18,22,22,12,15,39,61],
+#          [60,51,71,17,30,44,7,37,69,55,48,55],
+#          [11,4,61,31,41,25,28,70,64,8,58,9],
+#          [20,59,10,53,68,38,44,28,19,37,71,67],
+#          [24,13,68,33,32,42,3,20,49,27,40,34],
+#          [60,58,21,26,5,63,34,35,43,14,56,49],
+#          [24,33,6,56,50,1,66,0,2,10,23,57],
+#          [21,36,18,63,19,66,45,6,7,0,5,11],
+#          [3,62,50,15,53,62,25,64,35,67,42,38]])
 
-def pair_positions(board):
-    pos = defaultdict(list)
-    for i,v in enumerate(board):
-        pos[v].append(i)
-    return pos
+board = np.array([[5,5,7,2],[0,3,6,0],[4,7,2,4],[1,6,3,1]])
 
-def solver():
-    garden = extract_entities(file_path=file_path, key_feature=key_feature)
-    rows, columns = garden.shape
-    for i in rows:
-        for j in columns:
-            if garden[i][j] != garden[i+1][j+1]:
-                rotate90(x_cord=0, y_cord=0, size=3, garden=garden)
-    
-    # print(temp)
-    # return temp
+def solver(board):
+    shape = board.shape[0]
+    total_pairs = (shape*shape)//2
+    paired = np.full((shape,shape), False, dtype=bool)
+    score = 0
+    ops = []
+    while score < total_pairs:
+        make_move = False
 
-def bfs():
-    return        
+        for i in range(shape):
+            for j in range(shape):
+                if not paired[i][j]:
+                    partner = find_partner(i,j, board, paired)
+                    if partner is None:
+                        paired[i][j] = True
+                        continue
+
+                    x,y = partner
+                    if abs(i-x) + abs(j-y) == 1:
+                        paired[i][j] = True
+                        paired[x][y] = True
+                        score += 1
+                        make_move = True
+                        break 
+                    block = select_block(i,j,x,y, shape)
+                    if block is not None:
+                        x_cord,y_cord,size = block
+                        board = rotate90(y_cord, x_cord, size, board)
+                        ops.append((x_cord,y_cord,size))
+                        if abs(i - x) + abs(j - y) == 1:
+                            paired[i, j] = True
+                            paired[x, y] = True
+                            score += 1
+                    make_move = True
+                    break  
+            if make_move:
+                break
+    return board, ops
+
+
+
 
 if __name__ == "__main__":
     # print(np.array(extract_entities(file_path=file_path,key_feature=key_feature)))
     # print(solver())
-    solver()
+    new_board, ops = solver(board=board)
+    print(f"Number of step: {len(ops)}")
+    print(new_board)
--- utils.py
+++ utils.py
@@ -1,5 +1,7 @@
 import json 
 import numpy as np
+from collections import defaultdict
+
 
 def extract_entities(file_path, key_feature):
     try:
@@ -19,3 +21,26 @@ def rotate90(x_cord, y_cord, size, garden):
     sub = garden[y_cord:y_cord+size, x_cord:x_cord+size]
     garden[y_cord:y_cord+size, x_cord:x_cord+size] = np.flip(sub.T, axis=1)
     return garden
+
+def find_partner(x,y, board, paired):
+    v = board[x][y]
+    for i in range(board.shape[0]):
+        for j in range(board.shape[1]):
+            if (i,j) != (x,y) and not paired[x][y] and board[i][j] == v:
+                return (i,j)
+    
+    return None
+
+def select_block(i1,j1,i2,j2,shape):
+    for k in range(2,shape+1):
+        row_min = max(0, min(i1,i2)-k+1)
+        row_max = min(max(i1,i2), shape-k)
+        col_min = max(0, min(j1,j2)-k+1)
+        col_max = min(max(j1,j2), shape-k)
+
+        for r in range(row_min, row_max+1):
+            for c in range(col_min, col_max+1):
+                if (r <= i1 < r + k and r <= i2 < r + k) and (c <= j1 < c + k and c <= j2 < c + k):
+                    return (r,c,k)
+    
+    return None
