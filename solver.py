6c2dea25e3ac89832292c65682ccde5958095989
An Nguyen <anngntien@gmail.com>
2025-10-22 22:21:03 +0700

    Refactor brute force

--- solver.py
+++ solver.py
@@ -1,24 +1,9 @@
 import numpy as np
 import json
-import os
-from utils import rotate90, find_partner, select_block 
-from collections import defaultdict
+from utils import rotate90, find_partner, check_distance, cal_distance, find_positions
 
 file_path = "./Problem/problem.json"
 key_feature = ["problem", "field", "entities"]
-
-# board= np.array([[47,16,39,48,51,69,8,59,2,70,30,52],
-#          [17,65,46,45,12,46,29,43,36,40,65,47],
-#          [54,1,4,26,54,27,32,57,41,23,29,13],
-#          [52,14,31,9,16,18,22,22,12,15,39,61],
-#          [60,51,71,17,30,44,7,37,69,55,48,55],
-#          [11,4,61,31,41,25,28,70,64,8,58,9],
-#          [20,59,10,53,68,38,44,28,19,37,71,67],
-#          [24,13,68,33,32,42,3,20,49,27,40,34],
-#          [60,58,21,26,5,63,34,35,43,14,56,49],
-#          [24,33,6,56,50,1,66,0,2,10,23,57],
-#          [21,36,18,63,19,66,45,6,7,0,5,11],
-#          [3,62,50,15,53,62,25,64,35,67,42,38]])
 
 board = np.array([[5,5,7,2],
                   [0,3,6,0],
@@ -26,6 +11,9 @@ board = np.array([[5,5,7,2],
                   [1,6,3,1]])
 
 def solver(board):
+    """
+    Using 2x2 only to rotate
+    """
     shape = board.shape[0]
     total_pairs = (shape*shape)//2
     paired = np.full((shape,shape), False, dtype=bool)
@@ -34,36 +22,44 @@ def solver(board):
     while score < total_pairs:
         for i in range(shape):
             for j in range(shape):
-                if paired[i][j]:
+                if paired[i][j] == True:
                     continue
-                    
-                partner = find_partner(i,j, board, paired)
-                if partner is None:
+                
+                ii, jj = find_partner(i, j, board, paired)
+                if check_distance(i,j,ii,jj):
                     paired[i][j] = True
-                    continue
-
-                x,y = partner
-                if abs(i-x) + abs(j-y) == 1:
-                    paired[i][j] = True
-                    paired[x][y] = True
+                    paired[ii][jj] = True
                     score += 1
-                    break
+                    continue
 
-                while abs(i-x) + abs(j-y) != 1:
-                    block = select_block(i,j,x,y,shape)
-                    if block is None:
+                dist = cal_distance(i,j,ii,jj)
+                while dist > 1:
+                    if abs(jj - j) > abs(ii - i):
+                        board = rotate90(jj-1, ii-1, 2, board)
+                        ops.append({"x": jj-1, "y": ii-1, "n": 2})
+                    if abs(jj - j) < abs(ii - i):
+                        board = rotate90(jj-1, ii, 2, board)
+                        ops.append({"x": jj-1, "y": ii, "n": 2})
+                    if jj > 0:
+                        board = rotate90(jj, ii-1, 2, board)
+                        ops.append({"x": jj, "y": ii-1, "n": 2})
+                    ii, jj = find_positions(i,j,board)
+                    if check_distance(i,j,ii,jj):
+                        paired[i][j] = True
+                        paired[ii][jj] = True
+                        score += 1
                         break
-                    r,c,k = block
-                    board = rotate90(i,j,k, board)
-                    ops.append((j,i,k))
-
-                if abs(i - x) + abs(j - y) == 1:
-                    paired[i, j] = True
-                    paired[x, y] = True
-                    score += 1 
+                    update_dist = cal_distance(i,j,ii,jj)
+                    if update_dist < dist:
+                        dist = update_dist
+    
     return board, ops
 
 if __name__ == "__main__":
-    new_board, ops = solver(board=board)
+    board = np.array([[5,5,7,2],
+                  [0,3,6,0],
+                  [4,7,2,4],
+                  [1,6,3,1]])
+    new_board, ops = solver(board)
     print(f"Number of step: {len(ops)}")
     print(new_board)
--- utils.py
+++ utils.py
@@ -21,6 +21,23 @@ def rotate90(x_cord, y_cord, size, garden):
     garden[y_cord:y_cord+size, x_cord:x_cord+size] = np.flip(sub.T, axis=1)
     return garden
 
+def check_distance(x1,y1,x2,y2):
+    if abs(x1-x2) + abs(y1-y2) == 1:
+        return True
+    return False
+
+def cal_distance(x1,y1,x2,y2):
+    return abs(x1-x2) + abs(y1-y2)
+
+def find_positions(i_dup,j_dup,board):
+    v = board[i_dup][j_dup]
+    shape = board.shape[0]
+    for i in range(shape):
+        for j in range(shape):
+            if (i,j) != (i_dup,j_dup) and board[i][j] == v:
+                return (i,j)
+    return None
+
 def find_partner(x,y, board, paired):
     v = board[x][y]
     for i in range(board.shape[0]):
@@ -28,9 +45,3 @@ def find_partner(x,y, board, paired):
             if (i,j) != (x,y) and not paired[i][j] and board[i][j] == v:
                 return (i,j)
     return None
-
-def select_block(i, j, x, y, shape):
-    dx, dy = x - i, y - j
-
-    
-    return (r, c, k)
